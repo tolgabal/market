110acb8380ceb4428c85f2d9271cbcff177472c3
gorkewmm <gorkemuysal2002@hotmail.com>
2025-03-18 16:20:34 +0300

    Tedarik modulu eklenir

---
+++ tedarik.py
@@ -0,0 +1,9 @@
+class tedarik:
+    def __init__(self,urun_adi):
+        self 
+        self.urun_adi = urun_adi            
+        
+    def siparisOngoru(self):
+        print(f"{self.urun_adi} ürününden az kaldı")
+        
+        
