b22d2144f85d64490c30e188fa4fde55b3344ad9
Dogukan Uckan <dogukanuckan@gmail.com>
2025-03-18 15:57:49 +0300

    akilli_kasa.py yazılır

---
+++ akilli_kasa.py
@@ -0,0 +1,27 @@
+import tarti
+
+
+class AkilliKasa:
+    def __init__(self, islem, urun, odeme, tutar, miktar, miktar_type):
+        self.islem = islem
+        self.urun = urun
+        self.odeme = odeme
+        self.odeme = tutar
+        self.miktar = miktar
+        self.miktar_type = miktar_type
+        self.tarti = tarti()
+    
+    def urun_satis(self):
+        print("Aldiginiz urunler: ", self.urun, "Miktar: ", self.miktar, self.miktar_type)
+        print("{self.odeme} şeklinde yapacaginiz odeme tutari: ", self.tutar)
+
+    def urun_iade(self):
+        print("Iade ettiginiz urunler: ", self.urun)
+        print("{self.tutar} odediginiz miktar iade ediliyor.")
+
+    
+    def urun_miktari(self):
+        if self.miktar_type == "kg":
+            self.tarti.kg = 5
+        elif self.miktar_type == "adet":
+            self.tarti.adet = 2
