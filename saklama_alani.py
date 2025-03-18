04e80d67322e3b357bd13d4b1ebf7b0d50685e9d
gorkewmm <gorkemuysal2002@hotmail.com>
2025-03-18 15:47:03 +0300

    sakalama_alani olustuuldu

---
+++ saklama_alani.py
@@ -0,0 +1,17 @@
+class saklama_alani:
+    def __init__(self,sicaklik=0,nem=0,doluluk_durumu=0):
+        self.sicaklik = sicaklik
+        self.nem = nem
+        self.doluluk_durumu = doluluk_durumu
+    
+    
+    def sicaklik(self):
+        print(self.sicaklik)
+    
+    def nem(self):
+        print(self.nem)
+          
+    def doluluk_durumu(self):    
+        print(self.doluluk_durumu)
+        
+        
